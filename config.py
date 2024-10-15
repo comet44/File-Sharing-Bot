688e4a9c8473fc8ca13025eec18fd8c5ea56b39b
Atharva Sonawane <111640636+comet44@users.noreply.github.com>
2024-10-15 20:18:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,6 +34,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 BOT_USERNM = os.environ.get("BOT_USERNM", "Neet_warrior_robot")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_GROUP= int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
